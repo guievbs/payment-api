c1ddba581d086283164503557d7d3ae4d8ae0ddb
Guilherme Sebastião <165939012+guievbs@users.noreply.github.com>
2024-08-29 14:53:44 -0300

    feat: adiciona modelo de transação:  `Transaction`

---
+++ src/models/Transaction.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const transactionSchema = new mongoose.Schema({
+  amount: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Transaction", transactionSchema);
