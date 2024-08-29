ed00be5891a6abfe877c7c7f2bab587e43eb2749
Guilherme Sebastião <165939012+guievbs@users.noreply.github.com>
2024-08-29 15:21:01 -0300

    feat: adciona configuração para o Swagger

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const app = require("./src/app");
+const app = require("./app");
+const swaggerSetup = require("./config/swagger");
+
+swaggerSetup(app);
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
